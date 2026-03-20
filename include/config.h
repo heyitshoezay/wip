310ac31b85b14e8c4f9779fd6ffb154b865dc392
keruds <chavezjoe4@gmail.com>
2026-03-20 10:30:07 -0700

    revert

--- include/config.h
+++ include/config.h
@@ -210,7 +210,7 @@
 //#define THUNDER_STORM_WEATHER_ELECTRIC_TERRAIN
 
 // FOG_SETS_MISTY_TERRAIN makes the Fog map header weather set permanent Misty Terrain in battle
-//#define FOG_WEATHER_MISTY_TERRAIN
+#define FOG_WEATHER_MISTY_TERRAIN
 
 // NATURAL_GIFT_POWER_GEN defines the power of Natural Gift based on generation. Gen 6 or higher are modernized values.
 #define NATURAL_GIFT_POWER_GEN GEN_LATEST
