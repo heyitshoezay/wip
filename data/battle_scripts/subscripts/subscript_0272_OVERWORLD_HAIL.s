250191a7b399f968f72f6640f94143f902769c5c
keruds <chavezjoe4@gmail.com>
2026-09-13 12:36:39 -0700

    overworld hail fix?

--- data/battle_scripts/subscripts/subscript_0272_OVERWORLD_HAIL.s
+++ data/battle_scripts/subscripts/subscript_0272_OVERWORLD_HAIL.s
@@ -4,6 +4,16 @@
 .data
 
 _000:
+.if SNOW_WARNING_GENERATION >= 9
+    PlayBattleAnimation BATTLER_CATEGORY_PLAYER, BATTLE_ANIMATION_WEATHER_SNOW
+    Wait 
+    // It started to snow!
+    PrintMessage 1439, TAG_NONE
+    Wait 
+    WaitButtonABTime 30
+    UpdateVar OPCODE_FLAG_ON, BSCRIPT_VAR_FIELD_CONDITION, FIELD_CONDITION_SNOW_PERMANENT
+    End 
+.else
     PlayBattleAnimation BATTLER_CATEGORY_PLAYER, BATTLE_ANIMATION_WEATHER_HAIL
     Wait 
     // It’s hailing!
@@ -11,4 +21,5 @@ _000:
     Wait 
     WaitButtonABTime 30
     UpdateVar OPCODE_FLAG_ON, BSCRIPT_VAR_FIELD_CONDITION, FIELD_CONDITION_HAIL_PERMANENT
-    End 
+    End
+.endif
