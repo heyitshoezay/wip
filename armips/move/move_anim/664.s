799a8da4b8bf55f6aaedad22413c235737a6017f
keruds <chavezjoe4@gmail.com>
2026-09-13 22:20:12 -0700

    added protect animations for other protect like moves and updated grunt rosters

--- armips/move/move_anim/664.s
+++ armips/move/move_anim/664.s
@@ -6,25 +6,28 @@
 .create "build/move/move_anim/0_664", 0
 //Baneful Bunker
 a010_664:
-    loadparticlefromspa 0, 202 //protect
-    waitparticle
-    loadparticlefromspa 1, 123 //toxic
-    waitparticle
-
-    playsepan 1990, -117
-    addparticle 1, 1, 3
-    addparticle 1, 2, 3//toxic
-
-    addparticle 0, 0, 17//protect
-    cmd37 6, 0, 1, 2, 0, 0, 0, "NaN", "NaN"
-    wait 20
-    shadeattackingmon 20, 4, 31
-    waitparticle
-
-
-    unloadparticle 0
+    initspriteresource
+    loadspriteresource 0
+    loadspriteresource 1
+    loadspriteresource 2
+    loadspriteresource 3
+    loadspritemaybe 4, 0, 0, 0
+    loadspritemaybe 5, 0, 1, 1
+    loadspritemaybe 6, 0, 2, 2
+    loadspritemaybe 7, 0, 3, 3
+    callfunction 78, 1, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    loadparticle 0, 202
     waitstate
+    unloadspriteresource
+    resetsprite 0
+    resetsprite 1
+    resetsprite 2
+    resetsprite 3
+    playsepan 1990, -117
+    addparticle 0, 0, 17
+    cmd37 6, 0, 1, 2, 0, 0, 0, "NaN", "NaN"
+    waitparticle
+    unloadparticle 0
     end
-
 
 .close
